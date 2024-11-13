9a30367cb470142948b1a41f7b7e23a5439598ad
lukagoderdzishvili <goderdzishvili21@gmail.com>
2024-11-13 17:28:00 +0400

    CHANGE: remove comments

--- src/gameConfig.ts
+++ src/gameConfig.ts
@@ -1,11 +1,8 @@
 import { ApplicationOptions } from "pixi.js";
 
-// const canvasSize: number = Math.min(innerWidth, innerHeight);
 export default {
     resolution: window.devicePixelRatio || 1,
     backgroundColor: '#000000',
     antialias: true,
-    // width: canvasSize,
-    // height: canvasSize,
     resizeTo: window
 } as Partial<ApplicationOptions>;
